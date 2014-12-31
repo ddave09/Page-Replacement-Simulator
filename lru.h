01a461edfc16c4750432482aa60fecb76347206b
Dave <dsdave1691@gmail.com>
2014-12-30 22:59:00 -0500

    Separated Page Replacement Algorithm from Simulator

---
+++ lru.h
@@ -0,0 +1,13 @@
+#ifndef lru
+#define lru
+#include<iostream>
+#include<map>
+#include<fstream>
+#include<stdio.h>
+#include<sstream>
+#include<cstdlib>
+#include<list>
+#include<algorithm>
+
+void LeastRecentlyUsed(char *file_name,string frame_table_size);
+#endif
